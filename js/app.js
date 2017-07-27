121677c0cdca18cc5b059f07e9fd2de9219bd4a7
oroborusm <juan.bustamante.dg@gmail.com>
2017-07-27 17:04:43 -0400

    animacion scrolltop ok

--- js/app.js
+++ js/app.js
@@ -31,6 +31,22 @@
   }
 
   //scrolltop e indicador
+  $('a.scroll').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			|| location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 100
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+
 
   var sections = $('.content');
   var sectionLinks = $('.section-link');
@@ -48,7 +64,6 @@
   animateMarker();
 
   $(window).scroll(function() {
-
     animateMarker();
   });
 
@@ -58,7 +73,7 @@
 
     sections.each(function(index, value) {
 
-      var divPosition = $(this).offset().top;
+      var divPosition = $(this).offset().top -125;
       if (divPosition <= currentScroll) {
 
         currentSectionIndex = index;
